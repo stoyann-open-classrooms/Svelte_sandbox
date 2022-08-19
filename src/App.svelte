0a00d1532cf5b4ea1497c9599a04da132a13609d
Stoyann <stoyann.openclassrooms@gmail.com>
2022-08-19 13:36:40 +1100

    first-commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+import FeedbackForm from './lib/FeedbackForm.svelte';
+
+import Feedbacklist from './lib/FeedbackList.svelte'
+import FeedbackStats from './lib/FeedbackStats.svelte';
+ let feedback = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+]
+
+$: count = feedback.length
+$: average = feedback.reduce((a, {rating}) => a + rating, 0) / feedback.length
+
+
+const deleteFeedback = (e) =>  {
+    const itemId = e.detail;
+    feedback = feedback.filter((item) =>  item.id != itemId)
+}
+</script>
+
+<main class="container">
+  <FeedbackForm/>
+  
+    
+<FeedbackStats {count} {average}/>
+<Feedbacklist {feedback} on:delete-feedback={deleteFeedback}/>
+ 
+</main>
+
+
